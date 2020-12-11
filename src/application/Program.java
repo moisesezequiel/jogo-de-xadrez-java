b56f1701167985e0b0f814cf80efde391c65a5db
Moises Ezequiel <contato.moisesezequiel@outlook.com>
2020-12-11 01:51:53 -0300

    primeira classe de posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Posicao pos =  new Posicao(3, 6);
+		System.out.println(pos);
 	}
 
 }
