9821eb30a338d243af2f278c49b5f24267600383
Moisés Ezequiel <contato.moisesezequiel@outlook.com>
2021-01-02 15:32:28 -0300

    adicionando codigos de cores para rodar no terminal do git

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,31 +1,59 @@
 package application;
 
+import xadrez.Cores;
 import xadrez.PeçaXadrez;
 
 public class UI {
-	
-	public static void imprimirTabuleiro(PeçaXadrez[][]peças) {
+
+	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+	public static void imprimirTabuleiro(PeçaXadrez[][] peças) {
 		for (int i = 0; i < peças.length; i++) {
-			System.out.print((8-i)+ " ");
+			System.out.print((8 - i) + " ");
 			for (int j = 0; j < peças.length; j++) {
-				imprimirPeça(peças[i][j]);
+				imprimirPeça(peças[i][j],false);
 			}
 			System.out.println();
 		}
 		System.out.print("  A B C D E F G H");
 	}
-	
-	
-	
-	//metodo para imprimir a peça do xadrez
-	private static void imprimirPeça(PeçaXadrez peça) {
-		if (peça == null) {
-			System.out.print("-");
-		}else {
-			System.out.print(peça);
+
+	// metodo para imprimir a peça do xadrez
+	private static void imprimirPeça(PeçaXadrez peça, boolean background) {
+		if (background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
 		}
-		
-		System.out.print(" ");
+    	if (peça == null) {
+            System.out.print("-" + ANSI_RESET);
+        }
+        else {
+            if (peça.getCores() == Cores.WHITE) {
+                System.out.print(ANSI_WHITE + peça + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_YELLOW + peça + ANSI_RESET);
+            }
+        }
+        System.out.print(" ");
 	}
 
 }
